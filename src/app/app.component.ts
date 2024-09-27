6ea3a8ea65da96ebcd4cb0c9776d17fce706d4a7
maggie <maggie850617@gmail.com>
2024-09-27 16:44:27 +0800

    standalone vs NgModule & lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { TestModule } from './test/test.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet, HelloComponent, TestModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-app-ng18';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'lazy-example',
+    loadComponent: () =>
+      import('./example/example.component').then((m) => m.ExampleComponent),
+  },
+];
